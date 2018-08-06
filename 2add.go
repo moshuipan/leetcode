bbe136687c6e186c7289643a0d316ef4593e9bfb
luom <luom@zcmlc.com>
2018-08-06 15:34:57 +0800

    2add

--- 2add.go
+++ 2add.go
@@ -1,10 +1,5 @@
 package leetcode
 
-import (
-	"fmt"
-	"math"
-)
-
 // 给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
 
 // 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
@@ -28,29 +23,45 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	v1 := 0
-	for i := 1; ; {
-		if l1 == nil {
-			break
-		}
-		v1 += l1.Val * i
-		i = i * 10
-		l1 = l1.Next
-	}
-	for i := 1; ; {
-		if l2 == nil {
-			break
-		}
-		v1 += l2.Val * i
-		i = i * 10
-		l2 = l2.Next
-	}
-	l := len(fmt.Sprint(v1))
 	l3 := &ListNode{}
 	tmp := l3
-	for i := 1; i <= l; i++ {
-		tmp.Val = v1 % int(math.Pow10(i))
-		v1 = (v1 - tmp.Val) / int(math.Pow10(i))
+	x, y := l1, l2
+	var add bool
+	for {
+		var v int
+		if add {
+			v = x.Val + y.Val + 1
+		} else {
+			v = x.Val + y.Val
+		}
+		add = (v / 10) > 0
+		tmp.Val = v % 10
+		x = x.Next
+		y = y.Next
+		if x == nil && y == nil {
+			tmp.Next = nil
+			if add {
+				tmp.Next = &ListNode{Val: 1}
+			}
+			break
+		}
+		if x == nil && y != nil {
+			if add {
+				add = false
+				x = &ListNode{Val: 1}
+			} else {
+				tmp.Next = y
+				break
+			}
+		} else if x != nil && y == nil {
+			if add {
+				add = false
+				y = &ListNode{Val: 1}
+			} else {
+				tmp.Next = x
+				break
+			}
+		}
 		tmp.Next = &ListNode{}
 		tmp = tmp.Next
 	}

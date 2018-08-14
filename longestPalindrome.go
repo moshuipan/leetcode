0762c570a96e75fa440aebecbfefa7595d23e2ce
luom <luom@zcmlc.com>
2018-08-14 14:15:15 +0800

    最长回文子串-Manacher算法

--- longestPalindrome.go
+++ longestPalindrome.go
@@ -1,5 +1,9 @@
 package leetcode
 
+import (
+	"strings"
+)
+
 // 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
 // 示例 1：
 // 输入: "babad"
@@ -40,4 +44,63 @@ func longestPalindrome(s string) string {
 		}
 	}
 	return maxs
+}
+
+// Manacher算法
+//     ----------P0--r--j--r-----M-----r--i--r--P1---------------
+//     --------r--P0----j-----r--M--r-----i----P1--r-------------
+//     ----------P0-----------M-----------P1---i------------
+func longestPalindromeByManacher(s string) string {
+	ns := "#"
+	for i := 0; i < len(s); i++ { // 初始化字符串,字符间加#
+		ns += string(s[i]) + "#"
+	}
+	l := len(ns)
+	pl := make([]int, l) // 最大回文串半径
+	maxI := 0            // 半径最大的回文串中心值下标
+	maxS := ""           // 最大子字符串
+	pl[0] = 1
+	for i := 1; i < l; i++ {
+		p := maxI + pl[maxI] - 1
+		if i >= p { // 依次判断回文
+			j := 1
+			for i-j >= 0 && i+j < l {
+				if ns[i-j] == ns[i+j] {
+					j++
+				} else {
+					break
+				}
+			}
+			j--
+			pl[i] = j + 1
+			// if pl[i] > 2 {
+			maxI = i
+			// }
+			if 2*pl[i]-1 > len(maxS) {
+				maxS = ns[i-j : i+j+1]
+			}
+		} else { // 找对称点
+			symmetryP := 2*maxI - i
+			if pl[symmetryP]-1 < p-i { // pl[i]=pl[symmetryP]
+				pl[i] = pl[symmetryP]
+			} else { // x>i+pl[symmetryP]-1,依次比对
+				j := p - i + 1
+				for i-j >= 0 && i+j < l {
+					if ns[i-j] == ns[i+j] {
+						j++
+					} else {
+						break
+					}
+				}
+				pl[i] = j
+				// if pl[i] > 2 {
+				maxI = i
+				// }
+				if 2*pl[i]-1 > len(maxS) {
+					maxS = ns[i-pl[i]+1 : i+pl[i]]
+				}
+			}
+		}
+	}
+	return strings.Replace(maxS, "#", "", -1)
 }
